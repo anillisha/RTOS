c1ba7da88a99bc01904de5b1e59872282ee8ed93
Anil Kumar <30392868+anillisha@users.noreply.github.com>
2019-06-09 00:47:01 +0530

    tutorial 1

---
+++ tutorial_1/main.c
@@ -0,0 +1,104 @@
+/********************************************************************************
+Project Name: Demo of Task Creation in Nuc029See
+Date: 12/1/2019
+Anil Kumar
+email:eceaa2@gmail.com
+******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "freertos_evr.h"
+#include "cmsis_compiler.h"
+#include "FreeRTOS.h"                   // ARM.FreeRTOS::RTOS:Core
+#include "task.h"                       // ARM.FreeRTOS::RTOS:Core
+#include "semphr.h"                     // ARM.FreeRTOS::RTOS:Core
+#include "event_groups.h"               // ARM.FreeRTOS::RTOS:Event Groups
+#include "stream_buffer.h"     
+
+#define mainDELAY_LOOP_COUNT		( 0xffff )
+
+/* The task functions. */
+void vTask1( void *pvParameters );
+void vTask2( void *pvParameters );
+
+
+void SYS_Init(void)
+{
+    /* Enable IP clock */
+    CLK->APBCLK = CLK_APBCLK_UART0_EN_Msk;
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk);
+    SYS->GPB_MFP |= SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD;
+}
+
+
+int main()
+{
+    int8_t ch;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Init UART0 to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+	  GPIO_SetMode(PB, BIT2, GPIO_PMD_OUTPUT);
+	  printf("Simple Demo Code\n\n");
+	/* Create the other task in exactly the same way. */
+	
+	xTaskCreate( vTask1, "Task 1", 100, NULL, 1, NULL );
+	xTaskCreate( vTask2, "Task 2", 50, NULL, 1, NULL );
+
+	/* Start the scheduler to start the tasks executing. */
+	vTaskStartScheduler();	
+	for( ;; );
+	return 0;
+
+  
+}
+void vTask1( void *pvParameters )
+{
+const char *pcTaskName = "Task 1 is running\r\n";
+volatile uint32_t ul;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+		printf(" TASK 1 HAS CREAT \r\n");
+
+		/* Delay for a period. */
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			;
+		}
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vTask2( void *pvParameters )
+{
+const char *pcTaskName = "Task 2 is running\r\n";
+volatile uint32_t ul;
+
+	/* As per most tasks, this task is implemented in an infinite loop. */
+	for( ;; )
+	{
+		/* Print out the name of this task. */
+			printf(" TASK 2 HAS CREAT \r\n");
+
+		/* Delay for a period. */
+		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
+		{
+			;
+		}
+	}
+}
